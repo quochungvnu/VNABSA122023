914d629f6a2644fa55cb2a239652b16cb556c824
Shaun-le <lehuuloi.cs@gmail.com>
2023-11-05 17:17:55 +0700

    LGTM!

--- Gas/main.py
+++ Gas/main.py
@@ -10,13 +10,12 @@ from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 from pytorch_lightning import seed_everything
 
-from transformers import AdamW, T5Tokenizer, AutoTokenizer, T5ForConditionalGeneration
+from transformers import AdamW, T5ForConditionalGeneration, T5Tokenizer, AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import get_linear_schedule_with_warmup
 
 from data_utils import ABSADataset
 from data_utils import write_results_to_log, read_line_examples_from_file
 from eval_utils import compute_scores
-
 
 logger = logging.getLogger(__name__)
 
@@ -34,7 +33,7 @@ def init_args():
                         help="The way to construct target sentence, selected from: [annotation, extraction]")
     parser.add_argument("--do_train", action='store_true', help="Whether to run training.")
     parser.add_argument("--do_eval", action='store_true', help="Whether to run eval on the dev/test set.")
-    parser.add_argument("--do_direct_eval", action='store_true', 
+    parser.add_argument("--do_direct_eval", action='store_true',
                         help="Whether to run direct eval on the dev/test set.")
 
     # Other parameters
@@ -47,7 +46,7 @@ def init_args():
     parser.add_argument('--gradient_accumulation_steps', type=int, default=1,
                         help="Number of updates steps to accumulate before performing a backward/update pass.")
     parser.add_argument("--learning_rate", default=3e-4, type=float)
-    parser.add_argument("--num_train_epochs", default=20, type=int, 
+    parser.add_argument("--num_train_epochs", default=20, type=int,
                         help="Total number of training epochs to perform.")
     parser.add_argument('--seed', type=int, default=42, help="random seed for initialization")
 
@@ -79,21 +78,22 @@ def init_args():
 
 
 def get_dataset(tokenizer, type_path, args):
-    return ABSADataset(tokenizer=tokenizer, data_dir='mydata', data_type=type_path,
-                       paradigm='annotation', task='tasd', max_len=128)
+    return ABSADataset(tokenizer=tokenizer, data_dir=args.dataset, data_type=type_path,
+                       paradigm=args.paradigm, task=args.task, max_len=args.max_seq_length)
 
 
 class T5FineTuner(pl.LightningModule):
     def __init__(self, hparams):
         super(T5FineTuner, self).__init__()
         self.hparams = hparams
-        self.model = T5ForConditionalGeneration.from_pretrained('VietAI/vit5-base')
-        self.tokenizer = AutoTokenizer.from_pretrained('VietAI/vit5-base')
+
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(hparams.model_name_or_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(hparams.model_name_or_path)
 
     def is_logger(self):
         return True
 
-    def forward(self, input_ids, attention_mask=None, decoder_input_ids=None, 
+    def forward(self, input_ids, attention_mask=None, decoder_input_ids=None,
                 decoder_attention_mask=None, labels=None):
         return self.model(
             input_ids,
@@ -123,7 +123,7 @@ class T5FineTuner(pl.LightningModule):
         tensorboard_logs = {"train_loss": loss}
         return {"loss": loss, "log": tensorboard_logs}
 
-    def training_epoch_end_self(self, outputs):
+    def training_epoch_end(self, outputs):
         avg_train_loss = torch.stack([x["loss"] for x in outputs]).mean()
         tensorboard_logs = {"avg_train_loss": avg_train_loss}
         return {"avg_train_loss": avg_train_loss, "log": tensorboard_logs, 'progress_bar': tensorboard_logs}
@@ -144,19 +144,22 @@ class T5FineTuner(pl.LightningModule):
         optimizer_grouped_parameters = [
             {
                 "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
-                "weight_decay": 0.0,
+                "weight_decay": self.hparams.weight_decay,
             },
             {
                 "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
                 "weight_decay": 0.0,
             },
         ]
-        optimizer = AdamW(optimizer_grouped_parameters, lr=3e-4, eps=1e-8)
+        optimizer = AdamW(optimizer_grouped_parameters, lr=self.hparams.learning_rate, eps=self.hparams.adam_epsilon)
         self.opt = optimizer
         return [optimizer]
 
     def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx, second_order_closure=None):
-        optimizer.step()
+        if self.trainer.use_tpu:
+            xm.optimizer_step(optimizer)
+        else:
+            optimizer.step()
         optimizer.zero_grad()
         self.lr_scheduler.step()
 
@@ -166,21 +169,22 @@ class T5FineTuner(pl.LightningModule):
 
     def train_dataloader(self):
         train_dataset = get_dataset(tokenizer=self.tokenizer, type_path="train", args=self.hparams)
-        dataloader = DataLoader(train_dataset, batch_size=8, drop_last=True, shuffle=True, num_workers=4)
+        dataloader = DataLoader(train_dataset, batch_size=self.hparams.train_batch_size, drop_last=True, shuffle=True,
+                                num_workers=4)
         t_total = (
-            (len(dataloader.dataset) // (8 * max(1, 0)))
-            // 1
-            * float(20)
+                (len(dataloader.dataset) // (self.hparams.train_batch_size * max(1, len(self.hparams.n_gpu))))
+                // self.hparams.gradient_accumulation_steps
+                * float(self.hparams.num_train_epochs)
         )
         scheduler = get_linear_schedule_with_warmup(
-            self.opt, num_warmup_steps=0.0, num_training_steps=t_total
+            self.opt, num_warmup_steps=self.hparams.warmup_steps, num_training_steps=t_total
         )
         self.lr_scheduler = scheduler
         return dataloader
 
     def val_dataloader(self):
         val_dataset = get_dataset(tokenizer=self.tokenizer, type_path="dev", args=self.hparams)
-        return DataLoader(val_dataset, batch_size=16, num_workers=4)
+        return DataLoader(val_dataset, batch_size=self.hparams.eval_batch_size, num_workers=4)
 
 
 class LoggingCallback(pl.Callback):
@@ -214,13 +218,13 @@ def evaluate(data_loader, model, paradigm, task, sents):
     """
     device = torch.device(f'cuda:{args.n_gpu}')
     model.model.to(device)
-    
+
     model.model.eval()
     outputs, targets = [], []
     for batch in tqdm(data_loader):
         # need to push the data to device
-        outs = model.model.generate(input_ids=batch['source_ids'].to(device), 
-                                    attention_mask=batch['source_mask'].to(device), 
+        outs = model.model.generate(input_ids=batch['source_ids'].to(device),
+                                    attention_mask=batch['source_mask'].to(device),
                                     max_length=128)
 
         dec = [tokenizer.decode(ids, skip_special_tokens=True) for ids in outs]
@@ -229,7 +233,8 @@ def evaluate(data_loader, model, paradigm, task, sents):
         outputs.extend(dec)
         targets.extend(target)
 
-    raw_scores, fixed_scores, all_labels, all_preds, all_preds_fixed = compute_scores(outputs, targets, sents, paradigm, task)
+    raw_scores, fixed_scores, all_labels, all_preds, all_preds_fixed = compute_scores(outputs, targets, sents, paradigm,
+                                                                                      task)
     results = {'raw_scores': raw_scores, 'fixed_scores': fixed_scores, 'labels': all_labels,
                'preds': all_preds, 'preds_fixed': all_preds_fixed}
     pickle.dump(results, open(f"{args.output_dir}/results-{args.task}-{args.dataset}-{args.paradigm}.pickle", 'wb'))
@@ -239,7 +244,7 @@ def evaluate(data_loader, model, paradigm, task, sents):
 
 # initialization
 args = init_args()
-print("\n", "="*30, f"NEW EXP: {args.task.upper()} on {args.dataset}", "="*30, "\n")
+print("\n", "=" * 30, f"NEW EXP: {args.task.upper()} on {args.dataset}", "=" * 30, "\n")
 
 seed_everything(args.seed)
 
@@ -247,42 +252,40 @@ tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
 
 # show one sample to check the sanity of the code and the expected output
 print(f"Here is an example (from dev set) under `{args.paradigm}` paradigm:")
-dataset = ABSADataset(tokenizer=tokenizer, data_dir=args.dataset, data_type='dev', 
+dataset = ABSADataset(tokenizer=tokenizer, data_dir=args.dataset, data_type='dev',
                       paradigm=args.paradigm, task=args.task, max_len=args.max_seq_length)
 data_sample = dataset[2]  # a random data sample
 print('Input :', tokenizer.decode(data_sample['source_ids'], skip_special_tokens=True))
 print('Output:', tokenizer.decode(data_sample['target_ids'], skip_special_tokens=True))
-
 
 # training process
 if args.do_train:
     print("\n****** Conduct Training ******")
     model = T5FineTuner(args)
 
-    #checkpoint_callback = pl.callbacks.ModelCheckpoint(
-        #filepath=args.output_dir, prefix="ckt", monitor='val_loss', mode='min', save_top_k=3
-    #)
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        filepath=args.output_dir, prefix="ckt", monitor='val_loss', mode='min', save_top_k=3
+    )
 
-    # prepare for trainerbacth
+    # prepare for trainer
     train_params = dict(
         default_root_dir=args.output_dir,
         accumulate_grad_batches=args.gradient_accumulation_steps,
-        #gpus=args.n_gpu,
+        gpus=args.n_gpu,
         gradient_clip_val=1.0,
-        #amp_level='O1',
+        # amp_level='O1',
         max_epochs=args.num_train_epochs,
-        #checkpoint_callback=checkpoint_callback,
-        #callbacks=[LoggingCallback()],
+        checkpoint_callback=checkpoint_callback,
+        callbacks=[LoggingCallback()],
     )
 
     trainer = pl.Trainer(**train_params)
     trainer.fit(model)
 
     # save the final model
-    model.model.save_pretrained('/kaggle/working/checkpoints')
+    #model.model.save_pretrained(args.output_dir)
 
     print("Finish training and saving the model!")
-
 
 if args.do_eval:
 
@@ -305,13 +308,13 @@ if args.do_eval:
 
     # load dev and test datasets
     dev_dataset = ABSADataset(tokenizer, data_dir=args.dataset, data_type='dev',
-                    paradigm=args.paradigm, task=args.task, max_len=args.max_seq_length)
+                              paradigm=args.paradigm, task=args.task, max_len=args.max_seq_length)
     dev_loader = DataLoader(dev_dataset, batch_size=32, num_workers=4)
 
-    test_dataset = ABSADataset(tokenizer, data_dir=args.dataset, data_type='test', 
-                    paradigm=args.paradigm, task=args.task, max_len=args.max_seq_length)
+    test_dataset = ABSADataset(tokenizer, data_dir=args.dataset, data_type='test',
+                               paradigm=args.paradigm, task=args.task, max_len=args.max_seq_length)
     test_loader = DataLoader(test_dataset, batch_size=32, num_workers=4)
-    
+
     for checkpoint in all_checkpoints:
         epoch = checkpoint.split('=')[-1][:-5] if len(checkpoint) > 1 else ""
         # only perform evaluation at the specific epochs ("15-19")
@@ -324,7 +327,7 @@ if args.do_eval:
             model_ckpt = torch.load(checkpoint)
             model = T5FineTuner(model_ckpt['hyper_parameters'])
             model.load_state_dict(model_ckpt['state_dict'])
-            
+
             dev_result = evaluate(dev_loader, model, args.paradigm, args.task)
             if dev_result['f1'] > best_f1:
                 best_f1 = dev_result['f1']
@@ -360,7 +363,6 @@ if args.do_eval:
     log_file_path = f"{results_log_dir}/{args.task}-{args.dataset}.txt"
     write_results_to_log(log_file_path, test_results[best_step_metric], args, dev_results, test_results, all_epochs)
 
-
 # evaluation process
 if args.do_direct_eval:
     print("\n****** Conduct Evaluating with the last state ******")
@@ -373,19 +375,15 @@ if args.do_direct_eval:
     sents, _ = read_line_examples_from_file(f'data/{args.task}/{args.dataset}/test.txt')
 
     print()
-    test_dataset = ABSADataset(tokenizer, data_dir=args.dataset, data_type='test', 
-                    paradigm=args.paradigm, task=args.task, max_len=args.max_seq_length)
+    test_dataset = ABSADataset(tokenizer, data_dir=args.dataset, data_type='test',
+                               paradigm=args.paradigm, task=args.task, max_len=args.max_seq_length)
     test_loader = DataLoader(test_dataset, batch_size=32, num_workers=4)
     # print(test_loader.device)
     raw_scores, fixed_scores = evaluate(test_loader, model, args.paradigm, args.task, sents)
     # print(scores)
 
     # write to file
-    log_directory = "/kaggle/working/results_log/"
-    if not os.path.exists(log_directory):
-        os.makedirs(log_directory)
-
-    log_file_path = f"{log_directory}{args.task}-{args.dataset}.txt"
+    log_file_path = f"results_log/{args.task}-{args.dataset}.txt"
     local_time = time.asctime(time.localtime(time.time()))
     exp_settings = f"{args.task} on {args.dataset} under {args.paradigm}; Train bs={args.train_batch_size}, num_epochs = {args.num_train_epochs}"
     exp_results = f"Raw F1 = {raw_scores['f1']:.4f}, Fixed F1 = {fixed_scores['f1']:.4f}"
