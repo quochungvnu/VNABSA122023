416d69024d2a9edaa439c1b7a01237b91d2775e4
Shaun-le <lehuuloi.cs@gmail.com>
2023-11-07 23:43:07 +0700

    LGTM!

--- Multi-view/src/eval_utils.py
+++ Multi-view/src/eval_utils.py
@@ -12,15 +12,15 @@ def extract_spans_para(seq, seq_type):
     if seq_type == 'pred':
         for s in sents:
             try:
-                tok_list = ["[C]", "[S]", "[A]", "[O]"]
+                tok_list = ["[C ]", "[S ]", "[A ]", "[O ]"]
 
                 for tok in tok_list:
                     if tok not in s:
                         s += " {} null".format(tok)
-                index_ac = s.index("[C ]")
-                index_sp = s.index("[S ]")
-                index_at = s.index("[A ]")
-                index_ot = s.index("[O ]")
+                index_ac = s.index("[C]")
+                index_sp = s.index("[S]")
+                index_at = s.index("[A]")
+                index_ot = s.index("[O]")
 
                 combined_list = [index_ac, index_sp, index_at, index_ot]
                 arg_index_list = list(np.argsort(combined_list))
